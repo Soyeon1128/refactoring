4358029fae9ab7fe364d85aa95beaab24c880f39
Soyeon <soyeon1128@gmail.com>
2022-02-23 21:58:11 +0900

    format 변수를 함수로 추출, usd로 네이밍 변경

--- chapter1/statement.js
+++ chapter1/statement.js
@@ -4,23 +4,26 @@ module.exports = function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-  
-  const format = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minumumFractionDigits: 2
-  }).format;
 
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
-    result += ` ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience}석)\n`;
+    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
     totalAmount += amountFor(perf);
   }
 
-  result += `총액: ${format(totalAmount/100)}\n`;
+  result += `총액: ${usd(totalAmount)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
 
   return result;
+}
+
+// USD 단위로 포맷팅
+function usd(aNumber) {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minumumFractionDigits: 2
+  }).format(aNumber/100);
 }
 
 // 공연정보 가져오기
